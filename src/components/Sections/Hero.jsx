5729a78f60453b4b96ae39e5c7146af7cd83d62c
mayank.rawat <mayank.rawat@paytm.com>
2026-01-11 17:20:00 +0530

    Change hero text to 'Hi, I'm Mayank'

--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
           >
-            Hi, I'm <span className="text-gradient">{heroData.name}</span>
+            Hi, I'm <span className="text-gradient">Mayank</span>
           </motion.h1>
           
           <motion.p 
